00f62a0c86c46622aa4f9e8c1fbf4c13d629c313
wirocy7928 <61545848+wirocy7928@users.noreply.github.com>
2021-07-02 21:55:30 +0900

    CGV:MAIN

--- CGV/js/main.js
+++ CGV/js/main.js
@@ -1,10 +1,28 @@
-// // SECTION: BANNER;
+// SECTION: HEADER-SEARCH
+const searchEl = document.querySelector(".search");
+const searchInputEl = searchEl.querySelector("input");
+
+searchEl.addEventListener("click", function () {
+  searchInputEl.focus();
+});
+searchInputEl.addEventListener("focus", function () {
+  searchEl.classList.add("focused");
+  searchInputEl.setAttribute("placeholder", "통합검색");
+});
+searchInputEl.addEventListener("blur", function () {
+  searchEl.classList.remove("focused");
+  searchInputEl.setAttribute("placeholder", "");
+});
+
+// SECTION: BANNER;
 new Swiper(".banner .swiper-container", {
   slidesPerView: 3,
   centeredSlides: true,
-  spaceBetween:40,
+  spaceBetween: 40,
   loop: true,
-  autoplay: false,
+  autoplay: {
+    delay: 7000,
+  },
   pagination: {
     el: ".banner .swiper-pagination",
     clickable: true,
@@ -13,4 +31,23 @@ new Swiper(".banner .swiper-container", {
     prevEl: ".banner .swiper-prev",
     nextEl: ".banner .swiper-next",
   },
+});
+
+// SECTION: NOTICEBAR
+new Swiper(".notice_bar .mySwiper", {
+  autoplay: true,
+  loop: true,
+  direction: "vertical",
+});
+
+// SCROLL MAGIC
+const spyEl = document.querySelectorAll(".scroll-spy");
+spyEl.forEach(function (spyEl) {
+  new ScrollMagic.Scene({
+    //Scene은 특정요소 감시옵션 메소드
+    triggerElement: spyEl, //감시하다가 트리거(강제적으로 일으키는)
+    triggerHook: 0.8, //
+  })
+    .setClassToggle(spyEl, "show") //class를 빼주고 넣어주고 하는 메소드
+    .addTo(new ScrollMagic.Controller());
 });
